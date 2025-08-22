2f089fc7e164756d15fe30597929f95250f772aa
qs-vdjordjevic <vladimir.djordjevic@q-sit.de>
2025-08-22 18:37:25 +0200

    tcid13 removed sleeps

--- tests/my_account/test_register_new_user.py
+++ tests/my_account/test_register_new_user.py
@@ -17,14 +17,12 @@ class TestRegisterNewUser:
         my_acc_signed_in_page = MyAccountSignedIn(self.driver)
 
         my_account_page.go_to_my_account_page()
-        my_account_page.enter_registration_email("vlada2027@example.com")
-        time.sleep(3)
+        my_account_page.enter_registration_email("vlada2028@example.com")
         my_account_page.enter_registration_password("partizan2025!")
-        time.sleep(2)
         my_account_page.click_register_button()
         my_acc_signed_in_page.wait_until_logout_link_is_displayed()
-        breakpoint()
 
+    @pytest.mark.tcid14
     def test_verify_you_can_not_register_existing_user(self):
         expected_error_message = "An account is already registered with your email address"
         my_account_page = MyAccountSignedOut(self.driver)
@@ -36,6 +34,4 @@ class TestRegisterNewUser:
         my_account_page.click_register_button()
 
         my_account_page.wait_until_user_already_registered_error_is_displayed(expected_error_message)
-        # message_element = self.driver.find_element(By.XPATH, '//li[contains(., "An account is already registered with your email address")]')
-        # assert expected_error_message in message_element.text
         print("PASS")
