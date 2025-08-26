c1e20795a2ac1848b3e6be2940958f22ea90b80e
qs-vdjordjevic <vladimir.djordjevic@q-sit.de>
2025-08-26 13:51:13 +0200

    conftest updated

--- conftest.py
+++ conftest.py
@@ -5,22 +5,23 @@ import os
 
 @pytest.fixture(scope="class")
 def init_driver(request):
-    supported_browsers = ["chrome", "firefox"]
-
-    browser = os.environ.get("BROWSER")
-
-    if not browser:
-        raise Exception("The environment variable 'BROWSER' must be set")
-
-    browser = browser.lower()
-    if browser not in supported_browsers:
-        raise Exception(f"Browser {browser} is not one of supported."
-                        f"Supported are: {supported_browsers}")
-    if browser == "chrome":
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-    elif browser == "firefox":
-        driver = webdriver.Firefox()
+    # supported_browsers = ["chrome", "firefox"]
+    #
+    # browser = os.environ.get("BROWSER")
+    #
+    # if not browser:
+    #     raise Exception("The environment variable 'BROWSER' must be set")
+    #
+    # browser = browser.lower()
+    # if browser not in supported_browsers:
+    #     raise Exception(f"Browser {browser} is not one of supported."
+    #                     f"Supported are: {supported_browsers}")
+    # if browser == "chrome":
+    #     driver = webdriver.Chrome()
+    #     driver.maximize_window()
+    # elif browser == "firefox":
+    #     driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
 
     request.cls.driver = driver  # setting driver to a class variable
     yield
