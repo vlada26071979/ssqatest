54f29e95207a917252f9650b5c42191c23eaf9a1
qs-vdjordjevic <vladimir.djordjevic@q-sit.de>
2025-08-27 13:19:22 +0200

    report generation func added to yaml file

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pytest
+import pytest_html
 
 
 @pytest.fixture(scope="class")
@@ -26,3 +27,19 @@ def init_driver(request):
     request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    extras = getattr(report, "extras", [])
+    if report.when == "call":
+        # always add url to report
+        extras.append(pytest_html.extras.url("http://www.example.com/"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extras.append(pytest_html.extras.html("<div>Additional HTML</div>"))
+        report.extras = extras
