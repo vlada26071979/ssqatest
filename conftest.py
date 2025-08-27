f5d7f7f061d3760d9f10a6afb30c198daefbd4e0
qs-vdjordjevic <vladimir.djordjevic@q-sit.de>
2025-08-27 14:43:35 +0200

    update

--- conftest.py
+++ conftest.py
@@ -29,19 +29,19 @@ def init_driver(request):
     driver.quit()
 
 
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+@pytest.hookimpl(tryfirst=True, hookwrapper=False)
 def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
-    if rep.when == "call" and rep.failed:
-        driver = item.funcargs.get("init_driver")  # ili kako se tvoj driver zove
+    """ Hook that creates screenshot only when test fails """
+
+    if call.when == "call" and call.excinfo is not None:
+        driver = item.funcargs.get("init_driver")
         if driver:
             screenshots_dir = os.path.join(os.getcwd(), "test_reports", "screenshots")
             os.makedirs(screenshots_dir, exist_ok=True)
+
             timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             file_name = f"{item.name}_{timestamp}.png"
-            driver.save_screenshot(os.path.join(screenshots_dir, file_name))
-            print(f"\nScreenshot saved to {file_name}")
+            file_path = os.path.join(screenshots_dir, file_name)
 
+            driver.save_screenshot(file_path)
+            print(f"\nScreenshot saved to {file_path}")
